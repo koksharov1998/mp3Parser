4b37771ceb0d667d61d75dd46aaa393e498931a7
koksharov1998 <koksharov1998@ya.ru>
2018-12-21 03:57:52 +0500

    Can play music and stop it by button

--- mp3_parser.py
+++ mp3_parser.py
@@ -6,9 +6,13 @@ def parse_args():
     parser = argparse.ArgumentParser(description='User database utility')
     #parser.add_argument('-1', '--one', help='This will be option One')
     parser.add_argument('-n', '--name', nargs='?', help='This will be show name of test audio')
+    parser.add_argument('-p', '--play', nargs='?', help='Run music file')
     namespace = parser.parse_args(sys.argv[1:])
     if namespace.name:
         get_name(namespace.name)
+    elif namespace.play:
+        play(namespace.play)
+
 
 
 
@@ -41,7 +45,7 @@ def get_name(file):
     # play()
 
 
-def play():
+def play(file):
     import pygame
     pygame.init()
     pygame.display.set_caption('Player')
@@ -49,7 +53,7 @@ def play():
 
     pygame.mixer.init()
     pygame.mixer.music.set_volume(0.25)
-    pygame.mixer.music.load('music1.mp3')
+    pygame.mixer.music.load('musics/' + file)
     pygame.mixer.music.play()
 
     circle_color = (0, 0, 255)
@@ -59,6 +63,7 @@ def play():
     pygame.draw.circle(player, circle_color, circle_pos, circle_radius, circle_width)
 
     is_alive = True
+    playing = True
     while is_alive:
         player.fill((255, 255, 255))
         pause = pygame.draw.circle(player, circle_color, circle_pos, circle_radius, circle_width)
@@ -69,12 +74,19 @@ def play():
                 continue
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if pause.collidepoint(event.pos):
-                    pygame.mixer.music.pause()
+                    if playing:
+                        pygame.mixer.music.pause()
+                        playing = False
+                    else:
+                        pygame.mixer.music.unpause()
+                        playing = True
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_w:
                     pygame.mixer.music.unpause()
+                    playing = True
                 elif event.key == pygame.K_s:
                     pygame.mixer.music.pause()
+                    playing = False
                 elif event.key == pygame.K_d:
                     pygame.mixer.music.set_volume(pygame.mixer.music.get_volume() + 0.05)
                     print(pygame.mixer.music.get_volume())
@@ -87,6 +99,7 @@ def play():
 
 
 if __name__ == '__main__':
+    # play('music3.mp3')
     parse_args()
     #get_name()
     # main()
